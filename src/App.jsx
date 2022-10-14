984f3c912618fb37e244bb8128c0932f718b4379
Vinh Phat Tu <vphatmail@gmail.com>
2022-10-14 22:41:46 +0100

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
 import { useState } from 'react'
-import SearchIcon from '@mui/icons-material/Search';
+import './App.css';
 import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
+import InputAdornment from '@mui/material/InputAdornment'
+import Guitar from './components/Guitar';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <img 
-      style={{
-        maxWidth:'500px',
-        marginRight: '500px ',
-      }}
-      src='./src/assets/gglogo.png'/>
-      <TextField
-        id="input-with-icon-textfield"
-        label="Search"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
+    <div class="container">
+      <div class="logo">
+        <img src='./src/assets/gglogo.png'/>
+      </div>
+      <div class="search">
+        <TextField
+          id="we"
+          label="Search"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon/>
+              </InputAdornment>
+            )
+          }}
+          variant="standard"
+        />
+      </div>
+      <div class="guitar-1">
+        <Guitar/>
+      </div>
+      <div class="guitar-2">
+        <Guitar/>
+      </div>
+      <div class="guitar-3">
+        <Guitar/>
+      </div>
+      <div class="guitar-4">
+        <Guitar/>
+      </div>
+      <div class="guitar-5">
+        <Guitar/>
+      </div>
     </div>
   )
 }
---
+++ src/components/Guitar.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+const Guitar = () => {
+    return (
+        <img style={{maxWidth: '200px'}}
+        src="https://images.guitarguitar.co.uk/cdn/large/160/12050912030058f.jpg"/>
+    );
+}
+
+export default Guitar;
