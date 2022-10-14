3c81e3e3841b7f77939584d5620cd909a35cec69
Thomas Collins <thomas@localhost.localdomain>
2022-10-14 23:52:51 +0100

    Carousel Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react'
 import axios from 'axios';
+import React from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
+import { Arrow } from "./Arrow";
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
 
 function getData() {
   axios.get("http://localhost:5000/guitars")
@@ -9,11 +16,84 @@ function getData() {
   })
 }
 
-
 function App() {
-  const [count, setCount] = useState(0)
-  getData();
-  return <div></div>
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
+
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick =
+    (id) =>
+    ({ getItemById, scrollToItem }) => {
+      const itemSelected = isItemSelected(id);
+
+      setSelected((currentSelected) =>
+        itemSelected
+          ? currentSelected.filter((el) => el !== id)
+          : currentSelected.concat(id)
+      );
+    };
+
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {items.map(({ id }) => (
+        <Card
+          itemId={id} // NOTE: itemId is required for track items
+          title={id}
+          key={id}
+          onClick={handleClick(id)}
+          selected={isItemSelected(id)}
+        />
+      ))}
+    </ScrollMenu>
+  );
 }
 
-export default App
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } =
+    React.useContext(VisibilityContext);
+
+  return (
+    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+      Left
+    </Arrow>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+  return (
+    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+      Right
+    </Arrow>
+  );
+}
+
+function Card({ onClick, selected, title, itemId }) {
+  const visibility = React.useContext(VisibilityContext);
+
+  return (
+    <div
+      onClick={() => onClick(visibility)}
+      style={{
+        width: '160px',
+      }}
+      tabIndex={0}
+    >
+      <div className="card">
+        <div>{title}</div>
+        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div>
+      </div>
+      <div
+        style={{
+          height: '200px',
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
