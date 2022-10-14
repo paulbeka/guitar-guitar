cfa465e5ba8c344d2c7434e9b61ff0bb9c3a19b8
Vinh Phat Tu <vphatmail@gmail.com>
2022-10-14 21:03:50 +0100

    added logo + search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img 
+      style={{
+        maxWidth:'500px',
+        marginRight: '500px ',
+      }}
+      src='./src/assets/gglogo.png'/>
+      <TextField
+        id="input-with-icon-textfield"
+        label="Search"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
     </div>
   )
 }
