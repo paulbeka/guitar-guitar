5ca128490f3d4e6094b4e7b2b459d6f161d1f49a
Paul Bekaert <2517073B@student.gla.ac.uk>
2022-10-15 11:20:17 +0100

    added spotify stuff

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const CLIENT_ID = "68d571f08b8c432c8a88fb286b1fc191";
+const CLIENT_SECRET = "5b57c743669948d4ab76711e0ce62765";
+
 // basic string route to prevent Glitch error
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -15,11 +18,22 @@ app.get("/", (req, res) => {
 
 // the route we're working with
 app.get("/guitars", (req, res) => {
-    // replace with a custom URL as required
     const backendUrl = "https://services.guitarguitar.co.uk/WebService/api/hackathon/guitars";
-    // return the data without modification
     axios.get(backendUrl).then(response => res.send(response.data));
 });
+
+app.get("/search-spotify-songs", (req, res) => {
+    const backendUrl = "https://api.spotify.com/v1/search";
+    axios.post(backendUrl, {}, {
+        headers: {
+            'Authorization' : CLIENT_SECRET,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => {
+        res.send(response.data);
+    })
+})
 
 // console text when app is running
 app.listen(port, () => {
