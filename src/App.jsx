a9795be1adfb29c018b19f24783e2e76d7aaf6c3
Thomas Collins <thomas@localhost.localdomain>
2022-10-15 01:29:33 +0100

    Searching first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 
+const searchString = "Gretsch";
 
-function getData() {
-  axios.get("http://localhost:5000/guitars")
-  .then(res => {
-    console.log(res);
-  })
+
+
+function search(searchString){
+  return searchString;
 }
 
-
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const brandName = async () => {
+      await axios.get("http://localhost:5000/guitars")
+      .then(
+        res => setData(res.data))
+    }
+    brandName();
+  }, []);
+
   const [count, setCount] = useState(0)
-  getData();
-  return <div></div>
+  search(searchString);
+  return <div>{data[0].brandName}</div>
 }
 
 export default App
