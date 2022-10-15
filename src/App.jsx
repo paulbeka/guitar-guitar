4fe173c7e0e8da4259730146d97e7a9fcfc9768c
Thomas Collins <thomas@localhost.localdomain>
2022-10-15 07:24:19 +0100

    Searching update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect} from 'react';
 import axios from 'axios';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+import './App.css'
 
 const searchString = "Rancher";
 const dropString = "Item Name";
 let guitars = [];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 
 function search(searchString, dropString){
   let results = new Set();
@@ -20,14 +38,13 @@ function search(searchString, dropString){
 }
 
 function App() {
-  
   const url = 'http://localhost:5000/guitars';
   const [result, setResult] = useState(null);
 
   useEffect(() => {
     axios.get(url)
     .then((response)=>{
-      setResult(response.data)
+      setResult(response.data);
     })
   }, [])
 
@@ -36,12 +53,15 @@ function App() {
   } else {
     guitars = result;
   }
-
+  
   let searchResult = search(searchString, dropString);
-  console.log(searchResult);
   for (let i = 0; i < searchResult.size; i++){
     console.log([...searchResult][i].itemName);
-  }
+}
+
+  return(
+  <div className="App"></div>
+  )
 };
 
 export default App
